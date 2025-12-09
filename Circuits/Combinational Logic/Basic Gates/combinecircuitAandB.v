5b99ed10bacadb55b05804559faef9fde04bbf09
ananyaPal2001 <ananyapal@iisc.ac.in>
2025-12-09 23:54:52 +0530

    comb - basic gates
    combinational gates

---
+++ Circuits/Combinational Logic/Basic Gates/combinecircuitAandB.v
@@ -0,0 +1,16 @@
+module top_module (input x, input y, output z);
+    wire z1, z2, z3, z4;
+    A IA1(.x(x), .y(y), .z(z1));
+    A IA2(.x(x), .y(y), .z(z2));
+    B IB1(.x(x), .y(y), .z(z3));
+    B IB2(.x(x), .y(y), .z(z4));
+    assign z = (z1 | z3) ^ (z2 & z4);
+endmodule
+
+module A (input x, input y, output z);
+    assign z = ~y & x;
+endmodule
+
+module B (input x, input y, output z);
+    assign z = ~ (x ^ y);
+endmodule
---
+++ Circuits/Combinational Logic/Basic Gates/gatesv.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input [3:0] in,
+    output [2:0] out_both,
+    output [3:1] out_any,
+    output [3:0] out_different );
+    
+	assign out_any = in[3:1] | in[2:0];
+	assign out_both = in[2:0] & in[3:1];
+	// XOR 'in' with a vector that is 'in' rotated to the right by 1 position: {in[0], in[3:1]}
+	// The rotation is accomplished by using part selects[] and the concatenation operator{}.
+	assign out_different = in ^ {in[0], in[3:1]};
+
+endmodule
